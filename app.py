4ff778de338b18433dd6e92e790f386740ba33c7
Sai Ram Raju SIvangi <rajurama364@gmail.com>
2026-04-08 00:58:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from env import SmartAgriEnv
+
+app = FastAPI()
+env = SmartAgriEnv()
+
+@app.post("/reset")
+def reset():
+    obs = env.reset()
+    return {"observation": obs}
+
+@app.post("/step")
+def step(action: dict):
+    obs, reward, done, info = env.step(action)
+    return {
+        "observation": obs,
+        "reward": reward,
+        "done": done,
+        "info": info
+    }
+
+@app.get("/state")
+def state():
+    return {"state": env.state()}
+
+@app.get("/")
+def home():
+    return {"message": "Smart Agriculture OpenEnv is running"}
